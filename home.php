2a5c9ce576b631b5bf712a47d5d27bc89869a7c5
Juan Franco <juanframart2011@gmail.com>
2017-07-02 12:44:34 -0500

    Agregando boostrap a home.php

--- home.php
+++ home.php
@@ -11,6 +11,7 @@
         <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
         <title>Inicio | MeeTeam </title>
         <!-- Bootstrap -->
+        <link rel="stylesheet" href="css/bootstrap.css">
         <link href="style.css" rel="stylesheet">
         <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
         <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
